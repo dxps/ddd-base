705ea49ff2af47bcecc5951fc1a3ee6ce9e306fd
vision8 tech <vision8r@gmail.com>
2017-05-06 22:31:29 +0300

    small doc upd

--- src/main/java/tech/vision8/ddd/base/Entity.java
+++ src/main/java/tech/vision8/ddd/base/Entity.java
@@ -2,7 +2,7 @@ package tech.vision8.ddd.base;
 
 /**
  * Entity is one of the main DDD building blocks.<br/>
- * Any concrete entity as part of your specific domain model should implement this interface..
+ * Any concrete entity - part of your domain - should implement this interface.
  *
  * @param <Tid> The identifier's type.
  * @author vision8.tech
